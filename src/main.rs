52299f2b5d2282479e84c47c266d7e450bd874ba
Gustavo Barbosa <gusbdev@gmail.com>
2024-10-03 00:20:27 -0300

    refactor: reorganize main.rs and add CLI module

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,30 @@
+pub mod cli;
+pub mod git;
+pub mod changelog;
+pub mod file_utils;
+
 fn main() {
-    println!("Hello, world!");
+  let matches = cli::run_cli();
+    
+    match git::get_commits(".") {
+        Ok(commits) => {
+            let changelog;
+            if matches.args_present() {
+                changelog = changelog::generate_markdown_changelog(commits);
+            } else {
+                changelog = changelog::generate_changelog(commits);
+            }
+
+            if let Some(output) = matches.get_one::<String>("output") {
+                if let Err(e) = file_utils::save_to_file(&changelog, output) {
+                    eprintln!("Error saving changelog: {}", e);
+                } else {
+                    println!("Changelog saved to {}", output);
+                }
+            } else {
+                println!("{}", changelog);
+            }
+        },
+        Err(e) => eprintln!("Error: {}", e),
+    }
 }
